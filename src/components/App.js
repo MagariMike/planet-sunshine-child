abdce70413e5e9f659f1c4fad3ff454b4029cee5
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-05-31 22:52:47 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/app.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Planet Sunshine Child</h1>
+    </div>
+  );
+}
+
+export default App;
