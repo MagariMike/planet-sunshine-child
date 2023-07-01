abe1192c71d4798c8c2258a1f74ab21e379e4d42
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-07-02 00:05:57 +0100

    .name error resolved

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,65 +1,25 @@
-import React, { useState } from "react";
-import { storage } from "./firebase/config";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import "../styles/upload-form.css";
+import React from "react";
+import { useState } from "react";
 
 const UploadForm = () => {
-	const [uploadProgress, setUploadProgress] = useState(0);
-	const [file, setFile] = useState(null);
-	// const [url, setUrl] = useState(null);
-	// const [error, setError] = useState(null);
+	const [file, setFile] = useState();
 
 	const handleChange = (e) => {
 		setFile(e.target.files[0]);
-		console.log(file, file.name);
 	};
-
-	const uploadImage = (e) => {
-		const storageRef = ref(storage, `/${file.name}`);
-		const uploadTask = uploadBytesResumable(storageRef, file);
-
-		if (!file) {
-			alert("Please attach a file");
-		} else {
-			uploadTask.on(
-				"state_changed",
-				(snapshot) => {
-					const progress =
-						(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-					setUploadProgress(progress);
-					console.log(progress);
-				},
-				() => {
-					getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
-						console.log("Download Url:", downloadUrl);
-					});
-
-					// getDownloadURL(ref(storage, `/${file.name}`)).then((url) => {
-					// 	// setUrl(url);
-					// 	console.log(url);
-					// });
-				}
-			);
-		}
-
-		// getDownloadURL(ref(storage, `/${file.name}`)).then((url) => {setUrl(url);
-		// 	console.log(url);
-		// }).catch((/*err*/) => {
-		// 	// setError(err);
-		// })
-		// );
+	if(file) {console.log(file.name);}
+	
+	const uploadImage = () => {
+		
 	};
-
 	return (
-		<form id="upload-form">
+		<div>
 			<input
 				type="file"
 				accept="image/png, image/jpeg"
 				onChange={handleChange}
 			></input>
-			<button onClick={uploadImage}>Upload</button>
-			{/* <div>{progress} </div> */}
-		</form>
+		</div>
 	);
 };
 
