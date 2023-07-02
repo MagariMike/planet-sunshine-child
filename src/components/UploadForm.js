ed624a468a504fd79844dbe74ece91002a717caa
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-07-03 00:13:25 +0100

    onClick working upload

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from "react";
+import { storage } from "./firebase/config";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const UploadForm = () => {
 	const [file, setFile] = useState();
@@ -7,10 +9,32 @@ const UploadForm = () => {
 	const handleChange = (e) => {
 		setFile(e.target.files[0]);
 	};
-	if(file) {console.log(file.name);}
-	
-	const uploadImage = () => {
-		
+	if (file) {
+		console.log(file.name);
+	}
+
+	const uploadImage = (e) => {
+		const fileRef = ref(storage, `/${file.name}`);
+		const uploadTask = uploadBytesResumable(fileRef, file);
+
+		uploadTask.on(
+			"state_changed",
+			(snapshot) => {
+				const percentage =
+					(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+				// setProgress(percentage);
+				console.log("Upload is " + percentage + "% done");
+			},
+
+			getDownloadURL(ref(storage, `/${file.name}`))
+				.then((url) => {
+					// setUrl(url);
+					console.log(url);
+				})
+				.catch((err) => {
+					// setError(err);
+				})
+		);
 	};
 	return (
 		<div>
@@ -19,6 +43,8 @@ const UploadForm = () => {
 				accept="image/png, image/jpeg"
 				onChange={handleChange}
 			></input>
+			<button onClick={uploadImage}>Upload</button>
+			
 		</div>
 	);
 };
